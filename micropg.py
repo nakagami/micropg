5eb26440b94687fb36f35f740c10e374d351fae3
Hajime Nakagami <nakagami@gmail.com>
2017-10-30 16:51:39 +0900

    self md5 digest

--- micropg.py
+++ micropg.py
@@ -190,15 +190,124 @@ def _int_to_bytes(val):    # Convert int value to little endian 4 bytes.
     return bytes([val & 0xff, (val >> 8) & 0xff, (val >> 16) & 0xff, (val >> 24) & 0xff])
 
 
-def _md5_hexdigest(data):
+def _md5_hexdigest(message):
     try:
         import ubinascii
     except ImportError:
         # fallback for CPython
-        import hashlib
-        return hashlib.md5(data).hexdigest().encode("ascii")
+        import binascii as ubinascii
+        #import hashlib; print(hashlib.md5(message).hexdigest().encode("ascii"))
 
-    # TODO: ubinascii.hexlify(digest_bin)
+    # support only less than 56 bytes
+    assert len(message) < 56
+
+    def _bit_not(n): return ((1 << 32) - 1 - n) & 0xffffffff
+    def F(x,y,z):   return (z ^ (x & (y ^ z))) & 0xffffffff
+    def G(x,y,z):   return (y ^ (z & (y ^ x))) & 0xffffffff
+    def H(x,y,z):   return (x ^ y ^ z) & 0xffffffff
+    def I(x,y,z):   return (y ^ (x | _bit_not(z))) & 0xffffffff
+    def ROTATE_LEFT(x, n): return ((x << n) | (x >> (32-n))) & 0xffffffff
+    def FF(a, b, c, d, x, s, ac):
+        a = (a + F(b, c, d) + x + ac) & 0xffffffff
+        a = (ROTATE_LEFT(a, s) + b) & 0xffffffff
+        return a
+    def GG(a, b, c, d, x, s, ac):
+        a = (a + G(b, c, d) + x + ac) & 0xffffffff
+        a =  (ROTATE_LEFT(a, s) + b) & 0xffffffff
+        return a
+    def HH(a, b, c, d, x, s, ac):
+        a = (a + H(b, c, d) + x + ac) & 0xffffffff
+        a = (ROTATE_LEFT(a, s) + b) & 0xffffffff
+        return a
+    def II(a, b, c, d, x, s, ac):
+        a = (a + I(b, c, d) + x + ac) & 0xffffffff
+        a = (ROTATE_LEFT(a, s) + b) & 0xffffffff
+        return a
+
+    message_len = len(message)
+    data = message + b'\x80' + b'\00' * (55 - message_len) + _int_to_bytes(message_len*8) + b'\x00\x00\x00\x00'
+    W = [_bytes_to_int(b) for b in zip(*[iter(data)]*4)]
+    assert len(W) == 16
+
+    a0 = a = 0x67452301
+    b0 = b = 0xefcdab89
+    c0 = c = 0x98badcfe
+    d0 = d = 0x10325476
+
+    a = FF(a,b,c,d,W[0],7,0xd76aa478)
+    d = FF(d,a,b,c,W[1],12,0xe8c7b756)
+    c = FF(c,d,a,b,W[2],17,0x242070db)
+    b = FF(b,c,d,a,W[3],22,0xc1bdceee)
+    a = FF(a,b,c,d,W[4],7,0xf57c0faf)
+    d = FF(d,a,b,c,W[5],12,0x4787c62a)
+    c = FF(c,d,a,b,W[6],17,0xa8304613)
+    b = FF(b,c,d,a,W[7],22,0xfd469501)
+    a = FF(a,b,c,d,W[8],7,0x698098d8)
+    d = FF(d,a,b,c,W[9],12,0x8b44f7af)
+    c = FF(c,d,a,b,W[10],17,0xffff5bb1)
+    b = FF(b,c,d,a,W[11],22,0x895cd7be)
+    a = FF(a,b,c,d,W[12],7,0x6b901122)
+    d = FF(d,a,b,c,W[13],12,0xfd987193)
+    c = FF(c,d,a,b,W[14],17,0xa679438e)
+    b = FF(b,c,d,a,W[15],22,0x49b40821)
+
+    a = GG(a,b,c,d,W[1],5,0xf61e2562)
+    d = GG(d,a,b,c,W[6],9,0xc040b340)
+    c = GG(c,d,a,b,W[11],14,0x265e5a51)
+    b = GG(b,c,d,a,W[0],20,0xe9b6c7aa)
+    a = GG(a,b,c,d,W[5],5,0xd62f105d)
+    d = GG(d,a,b,c,W[10],9,0x02441453)
+    c = GG(c,d,a,b,W[15],14,0xd8a1e681)
+    b = GG(b,c,d,a,W[4],20,0xe7d3fbc8)
+    a = GG(a,b,c,d,W[9],5,0x21e1cde6)
+    d = GG(d,a,b,c,W[14],9,0xc33707d6)
+    c = GG(c,d,a,b,W[3],14,0xf4d50d87)
+    b = GG(b,c,d,a,W[8],20,0x455a14ed)
+    a = GG(a,b,c,d,W[13],5,0xa9e3e905)
+    d = GG(d,a,b,c,W[2],9,0xfcefa3f8)
+    c = GG(c,d,a,b,W[7],14,0x676f02d9)
+    b = GG(b,c,d,a,W[12],20,0x8d2a4c8a)
+
+    a = HH(a,b,c,d,W[5],4,0xfffa3942)
+    d = HH(d,a,b,c,W[8],11,0x8771f681)
+    c = HH(c,d,a,b,W[11],16,0x6d9d6122)
+    b = HH(b,c,d,a,W[14],23,0xfde5380c)
+    a = HH(a,b,c,d,W[1],4,0xa4beea44)
+    d = HH(d,a,b,c,W[4],11,0x4bdecfa9)
+    c = HH(c,d,a,b,W[7],16,0xf6bb4b60)
+    b = HH(b,c,d,a,W[10],23,0xbebfbc70)
+    a = HH(a,b,c,d,W[13],4,0x289b7ec6)
+    d = HH(d,a,b,c,W[0],11,0xeaa127fa)
+    c = HH(c,d,a,b,W[3],16,0xd4ef3085)
+    b = HH(b,c,d,a,W[6],23,0x04881d05)
+    a = HH(a,b,c,d,W[9],4,0xd9d4d039)
+    d = HH(d,a,b,c,W[12],11,0xe6db99e5)
+    c = HH(c,d,a,b,W[15],16,0x1fa27cf8)
+    b = HH(b,c,d,a,W[2],23,0xc4ac5665)
+
+    a = II(a,b,c,d,W[0],6,0xf4292244)
+    d = II(d,a,b,c,W[7],10,0x432aff97)
+    c = II(c,d,a,b,W[14],15,0xab9423a7)
+    b = II(b,c,d,a,W[5],21,0xfc93a039)
+    a = II(a,b,c,d,W[12],6,0x655b59c3)
+    d = II(d,a,b,c,W[3],10,0x8f0ccc92)
+    c = II(c,d,a,b,W[10],15,0xffeff47d)
+    b = II(b,c,d,a,W[1],21,0x85845dd1)
+    a = II(a,b,c,d,W[8],6,0x6fa87e4f)
+    d = II(d,a,b,c,W[15],10,0xfe2ce6e0)
+    c = II(c,d,a,b,W[6],15,0xa3014314)
+    b = II(b,c,d,a,W[13],21,0x4e0811a1)
+    a = II(a,b,c,d,W[4],6,0xf7537e82)
+    d = II(d,a,b,c,W[11],10,0xbd3af235)
+    c = II(c,d,a,b,W[2],15,0x2ad7d2bb)
+    b = II(b,c,d,a,W[9],21,0xeb86d391)
+
+    a = (a0 + a) & 0xffffffff
+    b = (b0 + b) & 0xffffffff
+    c = (c0 + c) & 0xffffffff
+    d = (d0 + d) & 0xffffffff
+
+    return ubinascii.hexlify(_int_to_bytes(a) + _int_to_bytes(b) + _int_to_bytes(c) + _int_to_bytes(d))
 
 
 class Error(Exception):
