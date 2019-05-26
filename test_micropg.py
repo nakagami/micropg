c19557961899c224528f64fc8e68e104a94476ce
Hajime Nakagami <nakagami@gmail.com>
2019-05-26 19:11:14 +0900

    add comment

--- test_micropg.py
+++ test_micropg.py
@@ -1,15 +1,19 @@
 import micropg
+
 conn = micropg.connect(
     host='127.0.0.1', user='postgres', password='', database='test_micropg'
 )
 
 cur = conn.cursor()
 
+# error
 try:
     cur.execute("BAD STATEMENT")
 except micropg.ProgrammingError as e:
     assert e.message == '42601:syntax error at or near "BAD"'
 
+
+# create table, insert, select
 try:
     cur.execute("DROP TABLE test_micropg")
 except:
@@ -30,6 +34,7 @@ cur.execute("SELECT id, name FROM test_micropg")
 assert cur.fetchall() == [(1, "test"), (2, "test2")]
 
 conn.close()
+
 
 # test ssl connection
 
