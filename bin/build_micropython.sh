3ed0e8b70459fc3127cc65da954ce6ea6515be26
Hajime Nakagami <nakagami@gmail.com>
2020-05-10 11:02:07 +0900

    fix build script (Ubuntsu 20.04)

--- bin/build_micropython.sh
+++ bin/build_micropython.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo apt install build-essential libreadline-dev libffi-dev git pkg-config python
+sudo apt install build-essential libreadline-dev libffi-dev git pkg-config
 git clone --recurse-submodules https://github.com/micropython/micropython.git micropython_src
 cd micropython_src/ports/unix
-make axtls
 make
 cp micropython ../../..
