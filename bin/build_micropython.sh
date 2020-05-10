0a1ff11bb36cd08078cc168debdd935522835d97
Hajime Nakagami <nakagami@gmail.com>
2020-05-10 11:06:49 +0900

    modify build script

--- bin/build_micropython.sh
+++ bin/build_micropython.sh
@@ -3,4 +3,3 @@ sudo apt install build-essential libreadline-dev libffi-dev git pkg-config
 git clone --recurse-submodules https://github.com/micropython/micropython.git micropython_src
 cd micropython_src/ports/unix
 make
-cp micropython ../../..
