7c785290b1425819a078c33bbcd5bae2858a8b2c
Hajime Nakagami <nakagami@gmail.com>
2021-11-20 00:10:52 +0900

    MicroPython can use hashlib.md5 now.

--- test_micropg.py
+++ test_micropg.py
@@ -38,11 +38,11 @@ conn.close()
 
 # test ssl connection
 
-# conn = micropg.connect(
-#     host='127.0.0.1', user='postgres', password='password', database='test_micropg', use_ssl=True
-# )
-# cur = conn.cursor()
-# cur.execute("SELECT id, name FROM test_micropg")
-# assert cur.fetchall() == [(1, "test"), (2, "test2")]
-#
-# conn.close()
+conn = micropg.connect(
+    host='127.0.0.1', user='postgres', password='password', database='test_micropg', use_ssl=True
+)
+cur = conn.cursor()
+cur.execute("SELECT id, name FROM test_micropg")
+assert cur.fetchall() == [(1, "test"), (2, "test2")]
+
+conn.close()
