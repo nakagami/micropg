d0db6938662e39377ec896b7d0ab6e4365228095
Hajime Nakagami <nakagami@gmail.com>
2021-11-20 00:18:16 +0900

    remove MicroPython build script

--- bin/build_micropython.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt install build-essential libreadline-dev libffi-dev git pkg-config
-git clone --recurse-submodules https://github.com/micropython/micropython.git micropython_src
-cd micropython_src/mpy-cross
-make
-cd ../ports/unix
-make
